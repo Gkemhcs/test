77e10a1fbc37071adf7bcfa6cf534add2497f303
Gkemhcs <gudikotieswarmani@gmail.com>
2023-11-18 13:44:28 +0000

    adde

--- app/server.py
+++ app/server.py
@@ -7,7 +7,7 @@ def home():
        return render_template("home.html",title=f"GITHUB-ACTIONS TO CLOUD-DEPLOY DEMO running in {environ.upper()} environment ",content="THIS PROJECTS SHOWS THE INTERGRATION OF GITHUB ACTIONS WITH CLOUD DEPLOY FOR CI/CD  ") 
 @app.route("/v1")
 def version():
-          return "I AM RUNNING VERSION V2"
+          return "I  AM RUNNING VERSION V2"
 @app.route("/health")
 def health_check():
         return "i am healthy"
